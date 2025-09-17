a4b428ffc3f722b13474d14f28e683ee440b5295
AlexCourtney18 <lex.courtney18@gmail.com>
2025-09-17 15:18:16 -0500

    set up drizzle db

--- server/api/tasks.get.ts
+++ server/api/tasks.get.ts
@@ -1,17 +1,6 @@
-export default defineEventHandler(async (event) => {
-    await new Promise(resolve => setTimeout(resolve, 2000));
-    return sendError(event, createError({
-        statusCode: 500,
-        statusMessage: 'Something went wrong',
-    }));
+import db from "~/lib/db";
 
-    return [{
-        id: 1,
-        title: 'Learn Nuxt',
-        done: false,
-    }, {
-        id: 2,
-        title: 'Build an app with Nuxt',
-        done: false,
-    }]
+export default defineEventHandler(async () => {
+	const tasks = await db.query.tasks.findMany();
+	return tasks;
 });
